7c96ca1a6e7e8ff00712e231e3ade4e8c5729814
Mishanea69 <mihai.olari.04@mail.ru>
2024-05-28 18:22:52 +0300

    ok

--- student1.cpp
+++ student1.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+class Test{
+    string nume;
+    string path;
+
+};
+
+class Grila: public Test{
+    
+};
+
+
+
 class brat{
     public:
     int ani;
