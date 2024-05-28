27875ddb3e5194e4923f0ec111523a0f84a97448
Mishanea69 <mihai.olari.04@mail.ru>
2024-05-28 23:54:22 +0300

    Am facut niste functii generale pentru a ne fi mai usor pe viitor

--- main.cpp
+++ main.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <conio.h>
+#include <initializer_list>
 #include "student1.cpp"
 #include "student2.cpp"
 using namespace std;
-int tata;
-int mama;
-int ma;
+
+
+string menu[] = {"View Tests", "Create New Test", "Exit"};
+
+void Menu(){
+    string selection;
+    while(1){
+        selection = InteractivePage("Main Menu", menu, 3);
+        if(selection=="Exit"){
+            if(ConfirmationPrompt("Are you sure?")) break;
+        }
+    }
+}
+
+
 int main()
 {
-    brat aha;
-    aha.ani = 2;
-    cout << aha.ani;    
-
-    //New change!!!!!
-
-    //Another change!
-    /// ia ibu!
+    Menu();
     return 0;
 }
--- student1.cpp
+++ student1.cpp
@@ -1,21 +1,144 @@
 #include <iostream>
 using namespace std;
 
+#define KEY_UP 72
+#define KEY_DOWN 80
+#define KEY_LEFT 75
+#define KEY_RIGHT 77
+#define ENTER 13
+
+
+
+
+class IntrebareGrila{
+    string intrebare;
+    char raspuns;
+public:
+    void print(){
+        cout << intrebare;
+    }
+
+};
+
+class IntrebareText{
+    string intrebare;
+    string raspuns;
+
+};
+
 class Test{
+public:
     string nume;
     string path;
 
+    void creazaTest(){
+        
+    }
 };
 
-class Grila: public Test{
-    
-};
+//Make a string coloured 
+string setColour(string str, string colour){
+    if(colour == "red"){
+        str = "\033[0;31m"+str+"\033[0m";
+    } else
+    if(colour == "green"){
+        str = "\033[0;32m"+str+"\033[0m";
+    } else
+    if(colour == "yellow"){
+        str = "\033[0;33m"+str+"\033[0m";
+    } else
+    if(colour == "blue"){
+        str = "\033[0;34m"+str+"\033[0m";
+    } else
+    if(colour == "purple"){
+        str = "\033[0;35m"+str+"\033[0m";
+    } else
+    if(colour == "cyan"){
+        str = "\033[0;36m"+str+"\033[0m";
+    } else
+    if(colour == "white"){
+        str = "\033[0;37m"+str+"\033[0m";
+    } else
+    if(colour == "black"){
+        str = "\033[0;30m"+str+"\033[0m";
+    }
+    return str;
 
+}
 
+//Display an interactive Page that returns the selection
+string InteractivePage(string title, const string menu[], int list_size, bool back_button = false){
+    int selection = 0;
+    auto showPage = [&](){
+        system("cls");
+        cout << setColour("---== "+title+" ==---\n", "cyan");
+        for(int i=0; i<list_size; i++){
+            if(selection == i) cout << setColour(">>"+menu[i], "red") << endl;
+            else cout << menu[i] << endl;
+        }
+        if(back_button){
+            cout << endl;
+            if(selection == list_size) cout << setColour("<--[Back]", "red") << endl;
+            else cout << "[Back]" << endl;
+        }
 
-class brat{
-    public:
-    int ani;
+        cout << "\n\n";
+    };
 
-};
+    showPage(); int ch;
+    while ((ch = getch()) != ENTER) {
+        switch (getch()) {
+            case KEY_UP:
+                if(selection > 0) {
+                    selection--;
+                }
+                break;
+            case KEY_DOWN:
+                if(selection < list_size) {
+                    selection++;
+                    if(!back_button && selection==list_size) selection--;
+                }
+                break;
+        }
+        showPage();
+    }
+    system("cls");
+    //front_hilight = selection;
+    if(selection<list_size) return menu[selection];
+    else return "";
+}
 
+bool ConfirmationPrompt(string question){
+    int ch; 
+    int pos=0;
+    auto ShowPage = [&](){
+        if(pos==0){
+            system("cls");
+            cout << setColour(question, "cyan") << endl << endl;
+            cout << setColour("[No]", "green") << "    [Yes]";
+        } else {
+            system("cls");
+            cout << setColour(question, "cyan") << endl << endl;
+            cout  << "[No]    " << setColour("[Yes]", "red");
+        }
+    };
+
+    ShowPage();
+    while ((ch = getch()) != ENTER) {
+        switch (getch()) {
+            case 75:
+                if(pos) {
+                    pos = 0;
+                }
+                break;
+            case 77:
+                if(!pos) {
+                    pos = 1;
+                }
+                break;
+        }
+        
+        ShowPage();
+    }
+    return pos;
+}
