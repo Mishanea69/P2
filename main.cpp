3c6c801f92723380dfa32c0945974471d8b82a29
Mishanea69 <mihai.olari.04@mail.ru>
2024-05-28 17:53:01 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "student1.cpp"
+#include "student2.cpp"
+using namespace std;
+
+
+
+
+int main()
+{
+    brat aha;
+    aha.ani = 2;
+    cout << aha.ani;    
+    
+    return 0;
+}
