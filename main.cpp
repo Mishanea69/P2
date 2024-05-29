5ff8f0dd5f441c3bb1e2580bbc118291959b4a2a
Uni Prowd <unihack235@gmai.com>
2024-05-29 03:20:54 +0300

    sazfa

--- main.cpp
+++ main.cpp
@@ -5,17 +5,20 @@
 #include <initializer_list>
 #include "student1.cpp"
 #include "student2.cpp"
+#include <fstream>
 using namespace std;
 
 
-string menu[] = {"View Tests", "Create New Test", "Exit"};
-
+string menu[] = {"View Tests", "Create New Test", "Statistici","Rulare","Exit"};
+string test="./data/teste/Test1/test.txt";
 void Menu(){
     string selection;
     while(1){
-        selection = InteractivePage("Main Menu", menu, 3);
+        selection = InteractivePage("Main Menu", menu, 4);
         if(selection=="Exit"){
             if(ConfirmationPrompt("Are you sure?")) break;
+        } else if(selection=="Rulare"){
+           ifTest_execute(test)
         }
     }
 }
--- student2.cpp
+++ student2.cpp
@@ -1,3 +1,19 @@
 #include <iostream>
 using namespace std;
 
+
+ void Test_execute(string file_name){
+        int nr_questions;
+        string line;
+        system('cls');
+        ifstream file(file_name);
+        if (!file.is_open()) { 
+        cerr << "Error opening the file!" << endl; 
+        exit(1);
+    }
+        cin>>nr_questions;
+        question_type_answer[nr_questions][1];
+        for(int i=0;i<nr_questions;++i){
+            
+        }
+    }
